4529f67eb36c73e7587d0ae9e5949d444d361a8a
paulmoschetti <paulmoschetti93@gmail.com>
2021-04-28 14:56:07 -0600

    birds controller include

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,7 +6,7 @@ class BirdsController < ApplicationController
   def index
     @birds = Bird.all
 
-    render json: @birds
+    render json: @birds, include: :characteristics
   end
 
   # GET /birds/1
