1fc15239dbbf25da9abc33a35e54e31298b6b3e5
paulmoschetti <paulmoschetti93@gmail.com>
2021-04-30 12:40:28 -0600

    bird styling

--- client/src/Screens/AllBirds/AllBirds.jsx
+++ client/src/Screens/AllBirds/AllBirds.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import './AllBirds.css';
 
 export default function AllBirds(props) {
   const { birds } = props;
   console.log(birds);
   return (
-    <div>
-    <h3>birds</h3>
-    {birds.map(bird => (
-      <React.Fragment key={bird.id}>
-        <Link to={`/birds/${bird.id}`}>
-          <p>{bird.name}</p>
-        <img src={bird.image_url} alt='bird'/>
-        </Link>
-      </React.Fragment>
-    ))}
-  </div>
-  )
+    <div className="allbird">
+      <div id="title">
+        <h3>birds</h3>
+      </div>
+      <div id='container'>
+
+      {birds.map((bird) => (
+        <React.Fragment key={bird.id}>
+          <Link id='bird' to={`/birds/${bird.id}`}>
+            <p id='bird-name'>{bird.name}</p>
+            <img id='bird-img' src={bird.image_url} alt="bird" />
+          </Link>
+        </React.Fragment>
+      ))}
+      </div>
+    </div>
+  );
 }
